a5ced70ef2ea925f6c1e6aa78900f4e09648de6e
Kieu Minh Canh <minhcanhdn0704@gmail.com>
2023-04-07 18:12:13 -0700

    Update demo page

--- dist/utils/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { NotifierMountComponent, ConfirmResult } from '../types';
-export declare const getVAppRoot: () => HTMLElement;
-export declare const createContainer: () => HTMLDivElement;
-export declare const mountComponent: ({ component, app, content, status, options }: NotifierMountComponent) => Promise<ConfirmResult>;
