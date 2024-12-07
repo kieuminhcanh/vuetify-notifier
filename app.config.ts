31eae90bf52c09446569396cdc1c234b49379704
Ken Kieu <minhcanhdn0704@gmail.com>
2024-12-07 12:19:53 -0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@
 export default defineAppConfig({
   docus: {
     title: 'Vuetify Notifier',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    description: 'Effortless Notifications & Dialogs for Vuetify.',
+    image: 'https://github.com/user-attachments/assets/6964387d-b685-4d8c-b5a3-e11d7ce9335b',
     socials: {      
       github: 'kieuminhcanh/vuetify-notifier',      
     },
