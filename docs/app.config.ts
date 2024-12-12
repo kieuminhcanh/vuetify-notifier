441c12180c48e5b10c785ee5a3147ed486fafdab
Ken Kieu <minhcanhdn0704@gmail.com>
2024-12-12 01:56:33 -0800

    Fix docs

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -4,30 +4,30 @@ export default defineAppConfig({
     title: 'Vuetify Notifier',
     description: 'Effortless Notifications & Dialogs for Vuetify.',
     image: 'https://github.com/user-attachments/assets/6964387d-b685-4d8c-b5a3-e11d7ce9335b',
-    socials: {
-      github: 'kieuminhcanh/vuetify-notifier',
+    socials: {      
+      github: 'kieuminhcanh/vuetify-notifier',      
     },
     github: {
       dir: 'content',
       branch: 'docs',
       repo: 'vuetify-notifier',
       owner: 'kieuminhcanh',
-      edit: true,
+      edit: true
     },
     aside: {
       level: 0,
       collapsed: false,
-      exclude: [],
+      exclude: []
     },
     main: {
       padded: true,
-      fluid: false,
+      fluid: false
     },
     header: {
       logo: false,
       showLinkIcon: true,
       exclude: [],
-      fluid: true,
-    },
-  },
+      fluid: true
+    }
+  }
 })
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,15 +1,16 @@
 export default defineNuxtConfig({
+  // https://github.com/nuxt-themes/docus
   extends: ['@nuxt-themes/docus'],
+  devtools: { enabled: false },
+
+  app: {
+    baseURL: process.env.NODE_ENV === 'development' ? '' : '/vuetify-notifier/'
+  },
+
   modules: [
     'vuetify-nuxt-module',
-    'vuetify-notifier/nuxt',
-    // '../src/module',
+    'vuetify-notifier/nuxt'
   ],
-  ssr: false,
-  app: {
-    baseURL: process.env.NODE_ENV === 'development' ? '' : '/vuetify-notifier/',
-  },
-  compatibilityDate: '2024-10-24',
   vuetify: {
     vuetifyOptions: {
       theme: {
@@ -17,4 +18,5 @@ export default defineNuxtConfig({
       },
     },
   },
+  compatibilityDate: '2024-10-24'
 })
