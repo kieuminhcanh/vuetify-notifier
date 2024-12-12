0bfc5c942247a2cea8273db3feb95878fa310d48
Ken Kieu <minhcanhdn0704@gmail.com>
2024-12-12 01:01:00 -0800

    Fix baseUrl

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   ],
   ssr: false,
   app: {
-    baseURL: process.env.NODE_ENV === 'development' ? '' : '/vuetify-notifier/',
+    // baseURL: process.env.NODE_ENV === 'development' ? '' : '/vuetify-notifier/',
   },
   compatibilityDate: '2024-10-24',
   vuetify: {
